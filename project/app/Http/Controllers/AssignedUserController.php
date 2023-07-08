79e0f2a297a5c8a1de1c614a4f7164c7c5fde10c
JoelMndz <luisjo3lml@gmail.com>
2023-07-07 21:57:59 -0500

    asignar usuarios y obtener usuarios

--- project/app/Http/Controllers/AssignedUserController.php
+++ project/app/Http/Controllers/AssignedUserController.php
@@ -7,43 +7,40 @@ use Illuminate\Http\Request;
 
 class AssignedUserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function create(Request $request)
     {
-        //
+        $rules = [
+            "user_id" => "required|integer",
+            "project_id" => "required|integer",
+            "isAdmin" => "required|boolean"
+        ];
+        $validator = \Validator::make($request->input(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "error" => $validator->errors()->all()
+            ], 400);
+        }
+
+        $exists = AssignedUser::where([
+            "user_id" => $request->user_id,
+            "project_id" => $request->project_id
+        ])->exists();
+
+        if ($exists) {
+            return response()->json([
+                "error" => "El usuario ya está asignado a este proyecto"
+            ], 400);
+        }
+
+        $assignedUser = new AssignedUser([
+            "user_id" => $request->user_id,
+            "project_id" => $request->project_id,
+            "isAdmin" => $request->isAdmin
+        ]);
+        $assignedUser->save();
+        
+        return response()->json($assignedUser);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(AssignedUser $assignedUser)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, AssignedUser $assignedUser)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(AssignedUser $assignedUser)
-    {
-        //
-    }
 }
--- project/routes/api.php
+++ project/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\AssignedUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -32,6 +35,14 @@ Route::middleware(['auth:sanctum'])->group(function () {
         Route::post('/project', 'create');
         Route::patch('/project/{project}', 'update');
         Route::delete('/project/{project}', 'delete');
+    });
+
+    Route::controller(AssignedUserController::class)->group(function () {
+        Route::post('/assigned-user', 'create');
+    });
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/user', 'getAll');
     });
 
     Route::get('/auth/logout', [AuthController::class, 'logout']);
